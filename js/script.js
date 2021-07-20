87e31410d0b2f64915f83937b4473ceb7035101a
Dipu <dipu.danish@outook.com>
2021-07-20 11:24:09 +0545

    about added

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@ $(window).on('load', function () {
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut();
 });
+
+$(document).ready(function () {
+    $("#team-members").owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots:false,
+        navText: ['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>']
+    });
+});
