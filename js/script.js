70008d76493f1de8f8013caf485321d0947c3b0a
Dipu <dipu.danish@outook.com>
2021-08-08 11:19:57 +0545

    work section added

--- js/script.js
+++ js/script.js
@@ -19,3 +19,14 @@ $(document).ready(function () {
 $('#services-tabs').responsiveTabs({
     startCollapsed: 'accordion'
 });
+$(window).on('load', function () {
+    $('#isotope-container').isotope({});
+    $('#isotope-filters').on('click', 'button', function () {
+        var filterValue = $(this).attr('data-filter');
+        $('#isotope-container').isotope({
+            filter: filterValue
+        });
+        $('#isotope-filters').find('.active').removeClass('active');
+        $(this).addClass('active');
+    });
+});
